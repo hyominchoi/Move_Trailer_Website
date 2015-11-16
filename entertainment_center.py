a91f0dfeaa129452ad669b4ab18a59ea1efb6509
hyominchoi <hyomin.choi@gmail.com>
2015-11-16 14:01:48 -0500

    Implement @property decorator in media.Movie() class

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,25 +5,29 @@ import media
 # title, storyline(imdb link), poster_url, youtube_url
 # class instance name is from its title in lower_cases
 
-erin_brockovich = media.Movie("Erin Brockovich",
-                              "http://www.imdb.com/title/tt0195685/",
-                              "http://www.gstatic.com/tv/thumb/movieposters/24917/p24917_p_v7_aa.jpg",
-                              "www.youtube.com/watch?v=9TjEklyF7-E")
+erin_brockovich = media.Movie()
+erin_brockovich.title = "Erin Brockovich"
+erin_brockovich.storyline = "http://www.imdb.com/title/tt0195685/"
+erin_brockovich.poster_image_url = "http://www.gstatic.com/tv/thumb/movieposters/24917/p24917_p_v7_aa.jpg"
+erin_brockovich.trailer_youtube_url = "www.youtube.com/watch?v=9TjEklyF7-E"
 
-hotel_rwanda = media.Movie("Hotel Rwanda",
-                           "http://www.imdb.com/title/tt0395169/?ref_=fn_al_tt_1",
-                           "http://www.gstatic.com/tv/thumb/movieposters/35240/p35240_p_v7_aa.jpg",
-                           "www.youtube.com/watch?v=qZzfxL90100")
+hotel_rwanda = media.Movie()
+hotel_rwanda.title = "Hotel Rwanda"
+hotel_rwanda.storyline = "http://www.imdb.com/title/tt0395169/?ref_=fn_al_tt_1"
+hotel_rwanda.poster_image_url = "http://www.gstatic.com/tv/thumb/movieposters/35240/p35240_p_v7_aa.jpg"
+hotel_rwanda.trailer_youtube_url = "www.youtube.com/watch?v=qZzfxL90100"
 
-camille_claudel = media.Movie("Camille Claudel",
-                              "http://www.imdb.com/title/tt2018086/?ref_=fn_al_tt_2",
-                              "https://upload.wikimedia.org/wikipedia/en/f/f6/Camille_Claudel_1915_poster.jpg",
-                              "https://youtu.be/UsB9QoMgr0I")
+camille_claudel = media.Movie()
+camille_claudel.title = "Camille Claudel"
+camille_claudel.storyline = "http://www.imdb.com/title/tt2018086/?ref_=fn_al_tt_2"
+camille_claudel.poster_image_url =  "https://upload.wikimedia.org/wikipedia/en/f/f6/Camille_Claudel_1915_poster.jpg"
+camille_claudel.trailer_youtube_url = "https://youtu.be/UsB9QoMgr0I"
 
-huit_femmes = media.Movie("8 Femmes",
-                          "http://www.imdb.com/title/tt0283832/?ref_=fn_al_tt_1",
-                          "https://upload.wikimedia.org/wikipedia/en/2/22/8-femmes-poster.jpg",
-                          "www.youtube.com/watch?v=CAxqXsao2Ew")
+huit_femmes = media.Movie()
+huit_femmes.title = "8 Femmes"
+huit_femmes.storyline = "http://www.imdb.com/title/tt0283832/?ref_=fn_al_tt_1"
+huit_femmes.poster_image_url = "https://upload.wikimedia.org/wikipedia/en/2/22/8-femmes-poster.jpg"
+huit_femmes.trailer_youtube_url = "www.youtube.com/watch?v=CAxqXsao2Ew"
 
 # movies is a list of movies from above
 movies = [erin_brockovich, hotel_rwanda, camille_claudel, huit_femmes]
--- media.py
+++ media.py
@@ -4,12 +4,45 @@ import webbrowser
 class Movie(object):
     """ This class stores movie information."""
     
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+    def __init__ (self):
+    	self._title = None
+    	self._storyline = None
+    	self._poster_image_url = None
+    	self._trailer_youtube_url = None
+
+    @property
+    def title(self):
+    	return self._title
+     
+    @title.setter
+    def title(self, value):
+    	self._title = value
+
+    @property
+    def storyline(self):
+    	return self._storyline
     
+    @storyline.setter
+    def storyline(self,value):
+    	self._storyline = value
+
+    @property
+    def poster_image_url(self):
+    	return self._poster_image_url
     
+    @poster_image_url.setter
+    def poster_image_url(self,value):
+    	self._poster_image_url = value
+
+    @property
+    def trailer_youtube_url(self):
+    	return self._trailer_youtube_url
+
+    @trailer_youtube_url.setter
+    def trailer_youtube_url(self,value):
+    	self._trailer_youtube_url = value
+
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+        webbrowser.open(self._trailer_youtube_url)
+
+
